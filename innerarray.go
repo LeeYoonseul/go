46aa79f1975f1e3ade11d564dfa576343c6332b5
root <dbstmf0331@naver.com>
2020-11-13 14:34:27 +0900

    version01

--- innerarray.go
+++ innerarray.go
@@ -4,15 +4,13 @@ import "fmt"
 
 
 func main(){
-	//array1 := [5]string{"i","n","h","a","!" }
 	array3 := [5]string{"a","b","c","d","e"}
-	slice3 := array3[0:3]
-	slice4 := array3[2:5]
-	fmt.Println(slice3, slice4)
-	//array3[2] = "z"
-	slice3[2] = "Queen"
-	//slice1 := array1[2:5]
-	fmt.Println(slice3, slice4)
-	fmt.Println(array3)
-	//fmt.Println(slice1)
+	//var slice3 []string
+	slice3 := make([]string,3)
+	slice3 = array3[0:3]
+
+	fmt.Println(slice3)
+	array3[2] = "Queen"
+	fmt.Println(slice3)
+	fmt.Printf("%x\n", &slice3[2])
 }
